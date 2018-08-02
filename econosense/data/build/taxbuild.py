8c6430d845bb8d39abe00353069135bea7cd5731
Ryan <rynkrn321@gmail.com>
2018-08-02 18:30:02 -0400

    Completed taxbuild process. Currently only populates Median Salaries

--- econosense/data/build/taxbuild.py
+++ econosense/data/build/taxbuild.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import datetime
+from datetime import datetime as dt
 import concurrent.futures
 
 #Set up Django Environment
@@ -47,7 +48,159 @@ class TaxBuild(Build):
         Tax.objects.filter(year=self.year).delete()
 
 
+
+
+    #This will first create gross and tax objects w/o associating them to a JobLocation
     def build(self):
+        self.refresh()
+        print('begin')
+
+        states = State.objects.filter(year=self.year).exclude(region__geo_id=9)
+        qs = JobLocation.objects.filter(year=self.year).filter(location_id__in=states).filter(median__gt=0).filter(median__lt=999999)
+
+        qs = qs.values('year','location','location__state__initials','median').distinct()
+        qs_len = len(qs)
+        print(qs_len)
+
+        gross_list = list()
+
+        start = dt.now()
+
+        batch_size = 5000
+        count = 0
+        #
+        futures = list()
+
+        if self.use_concurrency:
+
+            with concurrent.futures.ThreadPoolExecutor(max_workers=3) as pool:
+                futures = {pool.submit(self.build_helper,row): row for row in qs}
+
+                for future in concurrent.futures.as_completed(futures):
+                    try:
+                        gross = future.result()
+                    except:
+                        print(future.exception())
+
+                    gross_list.append(gross)
+
+                    if len(gross_list) >= batch_size:
+                        end = dt.now()
+                        print(end-start)
+                        print("Inserting batch of {} Gross objects".format(len(gross_list)))
+                        print('\n')
+
+                        Gross.objects.bulk_create(gross_list)
+                        gross_list.clear()
+
+                if len(gross_list) > 0:
+                    end = dt.now()
+                    print(end-start)
+                    print("Inserting batch of {} Gross objects".format(len(gross_list)))
+                    print('\n')
+
+                    Gross.objects.bulk_create(gross_list)
+
+                with django.db.connection.cursor() as cursor:
+                    sql = "update job_location set median_gross_id = gross.id from gross where job_location.year=gross.year and job_location.location_id=gross.state_id and job_location.median=gross.amount;"
+                    cursor.execute(sql)
+
+
+
+        else:
+            for row in qs:
+                count += 1
+
+                state = {
+                    'id'        : row['location'],
+                    'initials'  : row['location__state__initials']
+                }
+
+                gross = self.get_gross(state,row['median'])
+
+                if gross.has_null_tax():
+                    print(str(gross) + ' has null tax')
+
+                gross_list.append(gross)
+
+                #print(str(count) + ' / ' + str(qs_len))
+
+                if len(gross_list) >= batch_size:
+                    end = dt.now()
+                    print(end-start)
+                    print("Inserting batch of {} Gross objects".format(batch_size))
+                    print('\n')
+
+                    Gross.objects.bulk_create(gross_list)
+                    gross_list.clear()
+
+
+    def build_helper(self,row):
+        state = {
+            'id'        : row['location'],
+            'initials'  : row['location__state__initials']
+        }
+
+        gross = self.get_gross(state,row['median'])
+        if gross.has_null_tax():
+            print(str(gross) + ' has null tax')
+
+        return gross
+
+
+
+    def get_gross(self,state,pay_rate):
+        gross = Gross()
+
+        gross.year      = self.year
+        gross.state_id  = state['id']
+        gross.amount    = pay_rate
+
+
+        tax_list = list()
+
+        with concurrent.futures.ThreadPoolExecutor(max_workers=4) as pool:
+
+            futures = {pool.submit(self.get_tax,state,filing_status,pay_rate): filing_status for filing_status in self.filing_statuses}
+
+            for future in concurrent.futures.as_completed(futures):
+                try:
+                    tax = future.result()
+                except:
+                    print(future.exception())
+
+                if tax is not None:
+                    tax_list.append(tax)
+                    #gross.set_tax(tax)
+
+        Tax.objects.bulk_create(tax_list)
+        for tax in tax_list:
+            gross.set_tax(tax)
+
+        return gross
+
+
+    def get_tax(self,state,filing_status,pay_rate):
+        tax = Tax()
+
+        tax.year            = self.year
+        tax.state_id        = state['id']
+        tax.filing_status   = filing_status
+        tax.amount          = pay_rate
+
+        income_tax          = self.api.income_tax(self.year,pay_rate,filing_status,state['initials'])
+
+        tax.fica_tax        = income_tax.fica_tax
+        tax.state_tax       = income_tax.state_tax
+        tax.federal_tax     = income_tax.federal_tax
+
+        if tax.is_missing_info():
+            print('tax is missing info')
+
+        return tax
+
+
+    def build_old(self):
         self.refresh()
 
         states = State.objects.filter(year=self.year).exclude(region__geo_id=9)
@@ -76,7 +229,7 @@ class TaxBuild(Build):
                 ]
 
                 with concurrent.futures.ThreadPoolExecutor(3) as pool:
-                    futures = {pool.submit(self.get_gross,state,amount) : amount for amount in amounts}
+                    futures = {pool.submit(self.get_gross_old,state,amount) : amount for amount in amounts}
 
                     for future in concurrent.futures.as_completed(futures):
                         try:
@@ -111,7 +264,7 @@ class TaxBuild(Build):
 
 
 
-    def get_gross(self,state,pay_rate):
+    def get_gross_old(self,state,pay_rate):
         #Dont bother calculating tax for invalid values
         if pay_rate <= 0 or pay_rate >= 999999:
             return None
@@ -128,10 +281,11 @@ class TaxBuild(Build):
         gross.state = state
         gross.amount = pay_rate
 
+
         if self.use_concurrency:
             with concurrent.futures.ThreadPoolExecutor(max_workers=2) as pool:
 
-                futures = {pool.submit(self.get_tax,state,filing_status,pay_rate): filing_status for filing_status in self.filing_statuses}
+                futures = {pool.submit(self.get_tax_old,state,filing_status,pay_rate): filing_status for filing_status in self.filing_statuses}
 
                 for future in concurrent.futures.as_completed(futures):
                     try:
@@ -168,7 +322,7 @@ class TaxBuild(Build):
         return gross
 
 
-    def get_tax(self,state,filing_status,pay_rate):
+    def get_tax_old(self,state,filing_status,pay_rate):
         tax = Tax()
 
         tax.year = self.year
